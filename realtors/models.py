8e3863d4cf8d6759178f51500912b5542957a0d9
SumeetPatil7 <sumitpatil1299@gmail.com>
2021-12-02 16:35:43 +0530

    Add media url and models listing and realtor

---
+++ realtors/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import datetime
+
+class Realtor(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=datetime.now)
